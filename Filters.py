a128f1de44adbcb1a916a38b2b6ceedf683c7877
evelyyy <susloparova.anna@gmail.com>
2023-11-26 20:02:53 +0300

    add filters functions

---
+++ Filters.py
@@ -0,0 +1,40 @@
+def censur(text):
+
+    result = ""
+
+    for letter in text:
+
+        if letter == " ":
+
+            result += " "
+
+        else:
+
+            result += "*"
+
+    return result
+
+
+def heart(text):
+    result = 'heart'.join(text.split())
+
+    return result
+
+
+def smile(text):
+    result = 'smile'.join(text.split())
+
+    return result
+
+
+def loud(text):
+    result = text.upper()
+
+    return result
+
+
+def aestetic(text):
+    result = text.lower()
+
+    return result
+
