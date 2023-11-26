0ff1cda12ae6baaa8ebb5ff64112e737b2f5624c
evelyyy <susloparova.anna@gmail.com>
2023-11-26 20:17:16 +0300

    удалила пропуск строки

--- Filters.py
+++ Filters.py
@@ -13,26 +13,18 @@ def censur(text):
             result += "*"
 
     return result
-
-
 def heart(text):
     result = 'heart'.join(text.split())
 
     return result
-
-
 def smile(text):
     result = 'smile'.join(text.split())
 
     return result
-
-
 def loud(text):
     result = text.upper()
 
     return result
-
-
 def aestetic(text):
     result = text.lower()
 
